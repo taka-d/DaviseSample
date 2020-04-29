f94821a16ad905d755ce0cf724d13266e8198f84
徳久　貴裕 <tokuhisatakahiro@tokuhisatakahironoMacBook-Pro.local>
2020-04-29 20:04:39 +0900

    新規登録画面の修正とgitignore

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,12 +9,12 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
-  def move_to_index
-    redirect_to action: :index unless user_signed_in?
-  end
-
   def create
     Item.create(name: item_params[:name], price: item_params[:price], user_id: current_user.id)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 
   private
